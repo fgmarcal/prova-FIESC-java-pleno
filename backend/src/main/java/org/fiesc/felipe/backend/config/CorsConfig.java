7292f56c7f3bfe5c16d5647dce7db04e3231eeaa
fgmarcal <felipegarciamarcal@gmail.com>
2025-06-29 09:49:58 -0300

    feat: backend e api > implementa cors

---
+++ backend/src/main/java/org/fiesc/felipe/backend/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package org.fiesc.felipe.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Value("${frontend.base-url}")
+    private String allowedOrigins;
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(allowedOrigins));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        CorsConfigurationSource source = (CorsConfigurationSource) new UrlBasedCorsConfigurationSource();
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
